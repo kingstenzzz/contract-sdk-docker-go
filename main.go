8b44a9f71a700c81d619e824ebc6ab26dcfe88c7
bitvincent <896112698@qq.com>
2021-08-05 15:12:39 +0800

    style:fmt main.go

--- main.go
+++ main.go
@@ -33,10 +33,9 @@ func (t *TestContract) InvokeContract(stub shim.CMStubInterface) protogo.Respons
 		return t.GetTxIdFunc(stub)
 	}
 
-	if methodName == "getContractInfo"{
+	if methodName == "getContractInfo" {
 		return t.GetContractInfo(stub)
 	}
-
 
 	return shim.Error("unknown method")
 }
@@ -58,29 +57,28 @@ func (t *TestContract) Sum(stub shim.CMStubInterface) protogo.Response {
 }
 
 func (t *TestContract) GetTxIdFunc(stub shim.CMStubInterface) protogo.Response {
-	if txId,ok:=stub.GetTxId();ok!=nil{
+	if txId, ok := stub.GetTxId(); ok != nil {
 		return shim.Error("not found")
-	}else{
+	} else {
 		return shim.Success([]byte(txId))
 	}
 }
 
-func (t *TestContract) GetContractInfo(stub shim.CMStubInterface) protogo.Response{
-	creatorOrgId,_ := stub.GetCreatorOrgId()
-	creatorRole,_ := stub.GetCreatorRole()
-	creatorPk,_ := stub.GetCreatorPk()
-	senderOrgId,_ :=stub.GetSenderOrgId()
-	senderRole,_ :=stub.GetSenderRole()
-	senderPk,_:=stub.GetSenderPk()
-	blockHeight,_:=stub.GetBlockHeight()
-	txId,_:=stub.GetTxId()
-	info1 := fmt.Sprintf("creatorOrgId:[%v],creatorRole:[%v],creatorPk:[%v] ",creatorOrgId,creatorRole,creatorPk)
-	info2 := fmt.Sprintf("senderOrgId:[%v],senderRole:[%v],senderPk:[%v] ",senderOrgId,senderRole,senderPk)
-	info3 := fmt.Sprintf("blockHeight:[%v],txId:[%v]",blockHeight,txId)
-	info := info1+info2+info3
+func (t *TestContract) GetContractInfo(stub shim.CMStubInterface) protogo.Response {
+	creatorOrgId, _ := stub.GetCreatorOrgId()
+	creatorRole, _ := stub.GetCreatorRole()
+	creatorPk, _ := stub.GetCreatorPk()
+	senderOrgId, _ := stub.GetSenderOrgId()
+	senderRole, _ := stub.GetSenderRole()
+	senderPk, _ := stub.GetSenderPk()
+	blockHeight, _ := stub.GetBlockHeight()
+	txId, _ := stub.GetTxId()
+	info1 := fmt.Sprintf("creatorOrgId:[%v],creatorRole:[%v],creatorPk:[%v] ", creatorOrgId, creatorRole, creatorPk)
+	info2 := fmt.Sprintf("senderOrgId:[%v],senderRole:[%v],senderPk:[%v] ", senderOrgId, senderRole, senderPk)
+	info3 := fmt.Sprintf("blockHeight:[%v],txId:[%v]", blockHeight, txId)
+	info := info1 + info2 + info3
 	return shim.Success([]byte(info))
 }
-
 
 func main() {
 

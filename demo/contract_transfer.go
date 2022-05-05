3d2bb9ca900870def771be282738c3c1f3eefb78
liuxinfeng96 <liuxinfeng96@163.com>
2022-05-05 17:27:28 +0800

    chore: Add Copyright

--- demo/contract_transfer.go
+++ demo/contract_transfer.go
@@ -1,11 +1,19 @@
+/*
+Copyright (C) BABEC. All rights reserved.
+Copyright (C) THL A29 Limited, a Tencent company. All rights reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package demo
 
 import (
-	"chainmaker.org/chainmaker/chainmaker-contract-sdk-docker-go/pb/protogo"
-	"chainmaker.org/chainmaker/chainmaker-contract-sdk-docker-go/shim"
 	"fmt"
 	"log"
 	"strconv"
+
+	"chainmaker.org/chainmaker/chainmaker-contract-sdk-docker-go/pb/protogo"
+	"chainmaker.org/chainmaker/chainmaker-contract-sdk-docker-go/shim"
 )
 
 type TransferContract struct {
